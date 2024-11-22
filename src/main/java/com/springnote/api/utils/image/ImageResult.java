0c66f893a0a846f6910e5b67d2d575e2ec899a24
hrabit64 remote <hzser123@gmail.com>
2024-11-22 22:10:35 +0900

    [REFECTOR] image 기능 삭제
    image upload 및 serve 기능이 breezenote https://github.com/hrabit64/springnote-breezenote 로 이전됨에 따라
    메인api의 이미지 연관 기능은 모두 제거

--- src/main/java/com/springnote/api/utils/image/ImageResult.java
+++
@@ -1,7 +0,0 @@
-package com.springnote.api.utils.image;
-
-
-public record ImageResult(String originalFormat, String targetFormat, int width, int height,
-                          String name) {
-
-}
