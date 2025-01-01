a1e786ecc4838b0ad4aff59d18eb8213d60446ba
hrabit64 <hzser123@gmail.com>
2025-01-01 20:07:10 +0900

    [CHORE] recaptcha response 변경
    공식 문서에 따라 response 변경
    https://developers.google.com/recaptcha/docs/verify?hl=ko

--- src/main/java/com/springnote/api/security/captcha/RecaptchaResponse.java
+++ src/main/java/com/springnote/api/security/captcha/RecaptchaResponse.java
@@ -1,13 +1,16 @@
 package com.springnote.api.security.captcha;
 
-
+//{
+//        "success": true|false,
+//        "challenge_ts": timestamp,  // timestamp of the challenge load (ISO format yyyy-MM-dd'T'HH:mm:ssZZ)
+//        "hostname": string,         // the hostname of the site where the reCAPTCHA was solved
+//        "error-codes": [...]        // optional
+//        }
 public record RecaptchaResponse(
         boolean success,
-        double score,
-        String action,
-        String challengeTs,
+        String challenge_ts,
         String hostname,
-        String[] errorCodes
+        String[] error_codes
 ) {
 
 }
