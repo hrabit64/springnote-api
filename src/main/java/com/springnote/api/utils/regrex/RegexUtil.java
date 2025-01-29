df23adccf3896e0ed28ffb7c11357dc9f5cfc07a
hrabit64 <hzser123@gmail.com>
2025-01-29 14:18:25 +0900

    [FIX] URL 정규식 수정
    기존 정규식에서 빈 문자열을 허용하지 않아, 의도한대로 썸네일을 비어둔 값을 처리하지 못했음.
    이에 정규식을 수정함

--- src/main/java/com/springnote/api/utils/regrex/RegexUtil.java
+++ src/main/java/com/springnote/api/utils/regrex/RegexUtil.java
@@ -3,5 +3,5 @@ package com.springnote.api.utils.regrex;
 public class RegexUtil {
 
     public final static String PASSWORD_REGEX = "^(?=.*[a-zA-Z0-9!@#$%&*])[\\w!@#$%&*]{4,20}$";
-    public final static String URL_REGEX = "^(https?:\\/\\/)?(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)?$";
+    public final static String URL_REGEX = "^(https?:\\/\\/)?(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)?$|^$";
 }
